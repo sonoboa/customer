0cfe2d852a8f49057579574bc12db86e6dc09df7
Stalyn Noboa <sonoboa@hotmail.com>
2025-05-19 22:51:19 -0400

    final

--- src/app/add/page.jsx
+++ src/app/add/page.jsx
@@ -1,10 +1,7 @@
-import CustomersList from "./components/CustomersList";
-
 export default function Page() {
   return (
     <>
       <h1>Add Customer</h1>
-      <CustomersList />
     </>
   );
 }
